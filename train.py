8359d34a55c8998d2103037d9d4aba7968e8045e
trialan <thomasrialan@gmail.com>
2023-06-27 10:52:34 -0700

    .

--- train.py
+++ train.py
@@ -1,12 +1,12 @@
+import tqdm
 import glob
 import torch
 import matplotlib.pyplot as plt
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 
 from model import FaceAgeDataset, FaceAgeModel
 
 N_EPOCHS = 3
-
 
 if __name__ == '__main__': 
     image_paths = glob.glob('dataset/*.jpg')
@@ -18,7 +18,19 @@ if __name__ == '__main__':
     life_expectancies = [death - date for death, date in zip(death_dates, image_dates)]
 
     dataset = FaceAgeDataset(image_paths, ages, life_expectancies)
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+    # Split the dataset into training and test sets
+    test_ratio = 0.2  # Specifies what proportion of the dataset should be reserved for testing
+    num_test = int(test_ratio * len(dataset))
+    num_train = len(dataset) - num_test
+
+    train_dataset, test_dataset = random_split(dataset, [num_train, num_test])
+
+    # Define separate data loaders for the training and test sets
+    train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+    import pdb;pdb.set_trace() 
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -31,7 +43,9 @@ if __name__ == '__main__':
 
     # Training loop
     for epoch in range(N_EPOCHS):
-        for imgs, age, target in dataloader:
+        # Train
+        model.train()
+        for imgs, age, target in tqdm.tqdm(train_dataloader):
             imgs = imgs.to(device)
             age = age.to(device)
             target = target.to(device)
@@ -44,7 +58,17 @@ if __name__ == '__main__':
             loss.backward()
             optimizer.step()
 
-        print(f"Epoch {epoch+1}, Loss: {loss.item()}")
-    plt.plot(losses)
-    plt.show()
+        # Evaluate
+        model.eval()
+        test_loss = 0
+        with torch.no_grad():
+            for imgs, age, target in test_dataloader:
+                imgs = imgs.to(device)
+                age = age.to(device)
+                target = target.to(device)
 
+                output = model(imgs, age)
+                loss = criterion(output, target)
+                test_loss += loss.item()
+        
+        print(f"Epoch: {epoch+1}, Train Loss: {losses[-1]}, Test Loss: {test_loss / len(test_dataloader)}")
