c235ecfd0072b61dcef80f75d1f234b7cfaec96b
trialan <thomasrialan@gmail.com>
2023-07-04 01:54:04 -0700

    .

--- interpretability.py
+++ interpretability.py
@@ -5,9 +5,7 @@ import torch
 from train import _generate_dataset, _get_train_test_split
 
 
-def rescale_model_output_to_years(mse,
-                                  train_max_target,
-                                  train_min_target):
+def rescale_model_output_to_years(mse, train_max_target, train_min_target):
     rmse = math.sqrt(mse)
     rescaled =  rmse * (train_max_target - train_min_target)
     return rescaled
@@ -24,26 +22,3 @@ if __name__ == '__main__':
 
     rescale = lambda x: rescale_model_output_to_years(x, max_target, min_target)
 
-    
-
-
-
-
-
-
-    1/0
-
-
-    train_images = [train_dataset.dataset.image_paths[i] for i in train_dataset.indices]
-    train_lexs = [train_dataset.dataset.life_expectancies[i] for i in train_dataset.indices]
-    
-
-    def get_pic_year(f):
-        return f.split('data:')[1][:4]
-
-    def get_birth_year(f):
-        return f.split('birth:')[1][:4]
-
-
-    ixs = np.array([get_birth_year(f) == get_pic_year(f) for f in train_images])
-    print(sum(ixs))
